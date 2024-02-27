51dbbd5aca36f8cd5a2a626ccb8c2c930abbdf41
osampilona <brzicamartina@gmail.com>
2024-02-27 19:05:43 +0100

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { Menu, Item } from "./Menu";
+
+const rootElement = document.getElementById("root") ?? document.body;
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Menu
+      renderTrigger={(props) => <button {...props}>Actions</button>}
+      onAction={alert}
+      shouldFlip={false}
+    >
+      <Item key="copy">Copy application</Item>
+      <Item key="rename">Rename application</Item>
+      <Item key="move" title="Move to">
+        <Item key="move-to-shared">Shared</Item>
+        <Item key="move-to-desktop">Desktop</Item>
+        <Item key="move-to-favorite">Favorite</Item>
+      </Item>
+      <Item key="delete">Delete application</Item>
+    </Menu>
+  </StrictMode>
+);
