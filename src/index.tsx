fa712503913ce38fff14893544e863ccec009363
osampilona <brzicamartina@gmail.com>
2024-02-29 17:49:52 +0100

    Logic is done

--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,30 @@
-import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { Menu, Item } from "./Menu";
+import { ActionButton, Menu, Item, MenuTrigger } from "@adobe/react-spectrum";
 import { SubmenuTrigger } from "@react-spectrum/menu";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
 
-const App = () => {
-  const handleAction = (key: any) => {
-    if (key === "move") {
-      // Define your list items
-      const items = [
-        { key: "move-to-shared", label: "Shared" },
-        { key: "move-to-desktop", label: "Desktop" },
-        { key: "move-to-favorite", label: "Favorite" },
-      ];
-
-      // Convert the list of items into a string format
-      const itemsString = items.map((item) => `${item.label}`).join("\n");
-
-      // Display the string in an alert
-      alert(itemsString);
-    }
-  };
-
+function App() {
   return (
-    <StrictMode>
-      <Menu
-        renderTrigger={(props) => <button {...props}>Actions</button>}
-        onAction={handleAction}
-        shouldFlip={true}
-      >
-        <Item key="copy">Copy application</Item>
-        <Item key="rename">Rename application</Item>
-        <SubmenuTrigger>
-          <Item key="move" title="Move to">
-            Move to
-          </Item>
-          <Menu // Submenu with custom behavior
-            renderTrigger={(props) => (
-              <button onClick={() => handleAction("move")}>Move to...</button>
-            )} // Custom trigger for nested menu
-            shouldFlip={true} // Adjust as needed
-          >
-            <Item key="move-to-shared">Shared</Item>
-            <Item key="move-to-desktop">Desktop</Item>
-            <Item key="move-to-favorite">Favorite</Item>
-          </Menu>
-        </SubmenuTrigger>
-        <Item key="delete">Delete application</Item>
-      </Menu>
-    </StrictMode>
+    <Provider theme={defaultTheme} width={74}>
+      <MenuTrigger>
+        <ActionButton>Actions</ActionButton>
+        <Menu onAction={(key) => console.log(`Selected action: ${key}`)}>
+          <Item key="copy">Copy Application</Item>
+          <Item key="rename">Rename Application</Item>
+          <SubmenuTrigger>
+            <Item key="move">Move to</Item>
+            <Menu onAction={(key) => console.log(`Move to action: ${key}`)}>
+              <Item key="move-to-shared">Shared</Item>
+              <Item key="move-to-desktop">Desktop</Item>
+              <Item key="move-to-favorite">Favorite</Item>
+            </Menu>
+          </SubmenuTrigger>
+          <Item key="delete">Delete</Item>
+        </Menu>
+      </MenuTrigger>
+    </Provider>
   );
-};
+}
 
 const rootElement = document.getElementById("root") ?? document.body;
 const root = createRoot(rootElement);
